441f7891aa06a0302a9f945ae04910b121e03cad
Daniel <danek22@op.pl>
2020-03-25 18:05:26 +0100

    Update azure-signalr-service-sdk-asp-net-core-template
    Code cleanup

--- src/azure-signalr-service-sdk-asp-net-core-template/AzureDeveloperTemplates.SignalrSdk.WebAPI/Hubs/SampleHub.cs
+++ src/azure-signalr-service-sdk-asp-net-core-template/AzureDeveloperTemplates.SignalrSdk.WebAPI/Hubs/SampleHub.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
--- src/azure-signalr-service-sdk-asp-net-core-template/AzureDeveloperTemplates.SignalrSdk.WebAPI/Startup.cs
+++ src/azure-signalr-service-sdk-asp-net-core-template/AzureDeveloperTemplates.SignalrSdk.WebAPI/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AzureDeveloperTemplates.SignalrSdk.WebAPI.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AzureDeveloperTemplates.SignalrSdk.WebAPI
 {
