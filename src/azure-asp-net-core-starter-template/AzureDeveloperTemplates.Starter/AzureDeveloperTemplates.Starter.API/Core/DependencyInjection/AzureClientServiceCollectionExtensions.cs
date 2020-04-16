8182085c6db837754addb71c922921fae1fdbc5f
Daniel <danek22@op.pl>
2020-04-16 09:13:16 +0200

    Move CosmosClient initialization to data service collection extensions

--- src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Core/DependencyInjection/AzureClientServiceCollectionExtensions.cs
+++ src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Core/DependencyInjection/AzureClientServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Azure.Cosmos;
-using Azure.Cosmos.Serialization;
-using Azure.Storage.Blobs;
+﻿using Azure.Storage.Blobs;
 using AzureDeveloperTemplates.Starter.Infrastructure.Configuration.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -16,27 +14,6 @@ namespace AzureDeveloperTemplates.Starter.API.Core.DependencyInjection
             var storageConfiguration = serviceProvider.GetRequiredService<IStorageServiceConfiguration>();
             BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfiguration.ConnectionString);
             services.TryAddSingleton(blobServiceClient);
-
-            var cosmoDbConfiguration = serviceProvider.GetRequiredService<ICosmosDbDataServiceConfiguration>();
-            CosmosClientOptions cosmosClientOptions = new CosmosClientOptions
-            {
-                SerializerOptions = new CosmosSerializationOptions()
-                {
-                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-                }
-            };
-            CosmosClient cosmosClient = new CosmosClient(cosmoDbConfiguration.ConnectionString, cosmosClientOptions);
-            CosmosDatabase database = cosmosClient.CreateDatabaseIfNotExistsAsync(cosmoDbConfiguration.DatabaseName)
-                                                   .GetAwaiter()
-                                                   .GetResult();
-            CosmosContainer container = database.CreateContainerIfNotExistsAsync(
-                cosmoDbConfiguration.ContainerName,
-                cosmoDbConfiguration.PartitionKeyPath,
-                400)
-                .GetAwaiter()
-                .GetResult();
-
-            services.TryAddSingleton(cosmosClient);
 
             return services;
         }
--- src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Core/DependencyInjection/DataServiceCollectionExtensions.cs
+++ src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Core/DependencyInjection/DataServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using AzureDeveloperTemplates.Starter.Core.DomainModel;
+﻿using Azure.Cosmos;
+using Azure.Cosmos.Serialization;
+using AzureDeveloperTemplates.Starter.Core.DomainModel;
 using AzureDeveloperTemplates.Starter.Core.Services.Interfaces;
 using AzureDeveloperTemplates.Starter.Infrastructure.Configuration.Interfaces;
 using AzureDeveloperTemplates.Starter.Infrastructure.Services.Data;
@@ -12,6 +14,29 @@ namespace AzureDeveloperTemplates.Starter.API.Core.DependencyInjection
         public static IServiceCollection AddDataServices(this IServiceCollection services)
         {
             var serviceProvider = services.BuildServiceProvider();
+
+            var cosmoDbConfiguration = serviceProvider.GetRequiredService<ICosmosDbDataServiceConfiguration>();
+            CosmosClientOptions cosmosClientOptions = new CosmosClientOptions
+            {
+                SerializerOptions = new CosmosSerializationOptions()
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            };
+            CosmosClient cosmosClient = new CosmosClient(cosmoDbConfiguration.ConnectionString, cosmosClientOptions);
+            CosmosDatabase database = cosmosClient.CreateDatabaseIfNotExistsAsync(cosmoDbConfiguration.DatabaseName)
+                                                   .GetAwaiter()
+                                                   .GetResult();
+            CosmosContainer container = database.CreateContainerIfNotExistsAsync(
+                cosmoDbConfiguration.ContainerName,
+                cosmoDbConfiguration.PartitionKeyPath,
+                400)
+                .GetAwaiter()
+                .GetResult();
+
+            services.AddSingleton(cosmosClient);
+
+
             var sqlDbConfiguration = serviceProvider.GetRequiredService<ISqlDbDataServiceConfiguration>();
 
             services.AddDbContext<SqlDbContext>(c =>
