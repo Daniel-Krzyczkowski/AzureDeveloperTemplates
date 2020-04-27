317abbaa2d0421842cf54d58deee220a4699acb1
Daniel <danek22@op.pl>
2020-04-27 20:16:00 +0200

    Remove unnecessary using from the startup classs

--- src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Startup.cs
+++ src/azure-asp-net-core-starter-template/AzureDeveloperTemplates.Starter/AzureDeveloperTemplates.Starter.API/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace AzureDeveloperTemplates.Starter.API
 {
