fd0d1165a5d297e3abccb4b58da346cec512578a
Daniel <danek22@op.pl>
2020-05-11 12:45:08 +0200

    Add web app and API integrated with Azure AD B2C

---
+++ src/azure-ad-b2c-asp-net-core/AzureDeveloperTemplates.AdB2C/AzureDeveloperTemplates.AdB2C.WebAPI/Core/DependencyInjection/ConfigurationServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AzureDeveloperTemplates.AdB2C.WebAPI.Core.Configuration;
+using AzureDeveloperTemplates.AdB2C.WebAPI.Core.Configuration.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace AzureDeveloperTemplates.AdB2C.WebAPI.Core.DependencyInjection
+{
+    public static class ConfigurationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAppConfiguration(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<AzureAdB2cServiceConfiguration>(config.GetSection("AzureAdB2C"));
+            services.AddSingleton<IValidateOptions<AzureAdB2cServiceConfiguration>, AzureAdB2cServiceConfigurationValidation>();
+            var azureAdB2cServiceConfiguration = services.BuildServiceProvider().GetRequiredService<IOptions<AzureAdB2cServiceConfiguration>>().Value;
+            services.AddSingleton<IAzureAdB2cServiceConfiguration>(azureAdB2cServiceConfiguration);
+
+            return services;
+        }
+    }
+}
