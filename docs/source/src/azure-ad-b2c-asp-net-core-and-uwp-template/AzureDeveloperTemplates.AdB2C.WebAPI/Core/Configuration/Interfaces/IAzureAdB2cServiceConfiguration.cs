df69f2d173c7005eb876718b86e3e8a0bde44ac7
Daniel <danek22@op.pl>
2020-06-01 09:02:34 +0200

    Remove Azure AD B2C samples

--- docs/source/src/azure-ad-b2c-asp-net-core-and-uwp-template/AzureDeveloperTemplates.AdB2C.WebAPI/Core/Configuration/Interfaces/IAzureAdB2cServiceConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzureDeveloperTemplates.AdB2C.WebAPI.Core.Configuration.Interfaces
-{
-    public interface IAzureAdB2cServiceConfiguration
-    {
-        string Tenant { get; set; }
-        string ClientId { get; set; }
-        string Policy { get; set; }
-    }
-}
--- src/azure-ad-b2c-asp-net-core-and-uwp-template/AzureDeveloperTemplates.AdB2C.UWP/Services/Contract/AuthenticationData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AzureDeveloperTemplates.AdB2C.UWP.Services.Contract
-{
-    public class AuthenticationData
-    {
-        public string AccessToken { get; set; }
-    }
-}
